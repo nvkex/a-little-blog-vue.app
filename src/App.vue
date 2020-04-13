3369fe97ade225e76f8b92039742d9bb142ef58f
Sumit Bhardwaj <40798467+nvkex@users.noreply.github.com>
2020-04-13 20:24:07 +0530

    linked add-blog and show-blog

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <show-blog></show-blog>
   </div>
 </template>
 
 <script>
+import addBlog from './components/addBlog.vue'
+import showBlogs from './components/showBlogs.vue'
+
 export default {
-  name: 'app',
+  components:{
+    'add-blog':addBlog,
+    'show-blog':showBlogs
+  },
+
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+     
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+ body{
+    background: #2C3A47;
+    margin: 20px;
+    font-family: 'Gotu', sans-serif;
+    color:#f2f2f2;;
+    }
+  h1{ 
+      font-family: 'Work Sans', sans-serif;
+      color: #21deaf;
+    }
+  #showBlog, #addBlogDiv{
+        padding: 40px;
+        background: #3b4d5e;
+        max-width: 1100px;
+        margin:0 auto ;
+    }
 </style>
